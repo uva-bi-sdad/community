b9fcc64244633c3c82d050d1b8584c67a889d025
Alan Wang <22178748+wanghalan@users.noreply.github.com>
2023-03-28 10:00:38 -0400

    Update site.R

--- inst/templates/repository_site/site.R
+++ inst/templates/repository_site/site.R
@@ -18,7 +18,7 @@ page_section(
         min = "filter.time_min", max = "filter.time_max", default = "max", id = "selected_time"
       )
     ),
-    input_select("Layer", options = "datasets", default = 0, id = "selected_dataset"),
+    input_select("Layer", options = "datasets", default = "county", id = "selected_dataset"),
     "<br />",
     output_info(title = "variables.short_name"),
     input_button("Download", "export", query = list(include = "selected_variable"), class = "btn-full"),
